f6483beede90d8f2d843515094bd02bd9a65c16a
Nat <nat@cmmc.io>
2019-04-17 21:08:46 +0700

    send over nb-iot

--- lib/CMMC_Modem/CMMC_Modem.cpp
+++ lib/CMMC_Modem/CMMC_Modem.cpp
@@ -3,31 +3,12 @@
 #define AIS_TOKEN "76d514f0-217f-11e9-a028-9771a15972bf" // nat-devstation
 #include "coap.h"
 #include "coap-helper.h"
-// CMMC_PACKET_T pArr[60];
-int pArrIdx = 0;
-uint32_t lastSentOkMillis = 0;
-unsigned int ct = 1;
-static char msgId[5];
-IPAddress ip = IPAddress(103, 20, 205, 85);
-uint8_t _buffer[1300];
-float  batt;
-float  batt_raw;
-float  batt_percent;
-int analogValue;
-CMMC_Interval keepAliveInterval;
 RTC_DATA_ATTR int rebootCount = -1;
 void sendPacket(uint8_t *text, int buflen);
 enum {
-  TYPE_KEEP_ALIVE=1,
+  TYPE_KEEP_ALIVE = 1,
   TYPE_SENSOR_NODE
 } DATA_COAP_TYPE;
-
-char latC[20];
-char lngC[20];
-char latlngC[40];
-
-uint8_t currentSleepTimeMinuteByte = 30;
-uint32_t msAfterESPNowRecv = millis();
 
 CMMC_Modem::CMMC_Modem(Stream* s)   {
   this->_modemSerial = s;
@@ -86,11 +67,11 @@ void CMMC_Modem::setup() {
   static int counter;
   counter = 0;
   nb->onConnecting([]() {
-    counter = (counter+1) % 3;
+    counter = (counter + 1) % 3;
     // String t = "Attching";
     String t = "";
     for (size_t i = 0; i < counter; i++) {
-      t+= String(".");
+      t += String(".");
     }
     that->updateStatus(t);
     delay(500);
@@ -111,38 +92,91 @@ void CMMC_Modem::setup() {
 
 void CMMC_Modem::loop() {
   nb->loop();
-  keepAliveInterval.every_ms(5000, [&]() {
-      static char jsonBuffer[1024];
-      bzero(_buffer, sizeof(_buffer));
-      uint32_t uptime_s =  millis() / 1000;
+  static CMMC_Modem *that;
+  that = this;
+  keepAliveInterval.every_ms(1000, []() {
+    static char jsonBuffer[1024];
+    int pArrIdx = 0;
+    uint32_t lastSentOkMillis = 0;
+    unsigned int ct = 1;
+    static char msgId[5];
+    char latC[20];
+    char lngC[20];
+    char latlngC[40];
+
+    uint8_t currentSleepTimeMinuteByte = 30;
+    uint32_t msAfterESPNowRecv = millis();
+
+    IPAddress ip = IPAddress(103, 20, 205, 85);
+    uint8_t _buffer[1300];
+    float  batt;
+    float  batt_raw;
+    float  batt_percent;
+    int analogValue;
+    CMMC_Interval keepAliveInterval;
+
+    bzero(_buffer, sizeof(_buffer));
+    uint32_t uptime_s =  millis() / 1000;
     Serial.println("KEEP ALIVE INTERAL...");
     printf(">> CASE; keep alive..\n");
-    sprintf(jsonBuffer, "{\"loc\":\"%s\",\"reset\":%d,\"type\":%d,\"uptime_s\":%lu,\"heap\":%lu,\"batt\":%s,\"ct\":%lu,\"sleep\":%lu,\"payload\":\"%s\"}",
-              latlngC, rebootCount, TYPE_KEEP_ALIVE, uptime_s, ESP.getFreeHeap(), String(batt).c_str(), ct++, currentSleepTimeMinuteByte, "X");
+    sprintf(jsonBuffer, "{\"loc\":\"%s\",\"reset\":%d,\"type\":%d,\"uptime_s\":%lu,\"heap\":%lu,\"batt\":%s,\"ct\":%lu,\"sleep\":%lu,\"payload\":\"%s\"}", latlngC, rebootCount, TYPE_KEEP_ALIVE, uptime_s, ESP.getFreeHeap(), String(batt).c_str(), ct++, currentSleepTimeMinuteByte, "X");
     uint16_t buflen = generate(_buffer, ip, 5683, "NBIoT/" AIS_TOKEN, COAP_CON,
-      COAP_POST, NULL, 0, (uint8_t*) jsonBuffer, strlen(jsonBuffer));
-    Serial.println(jsonBuffer );
-    // sendPacket((uint8_t*)_buffer, buflen);
+                               COAP_POST, NULL, 0, (uint8_t*) jsonBuffer, strlen(jsonBuffer));
+    Serial.println(jsonBuffer);
+    that->sendPacket((uint8_t*)_buffer, buflen);
   });
 
-    // while (true) {
-    // updateStatus("dispatching queue...");
-    // if (nb.sendMessageHex(buffer, buflen, 0)) {
-    //   updateStatus(">> [ais] socket0: send ok.");
-    //   lastSentOkMillis = millis();
-    //   nbSentOk++;
-    //   delay(100);
-    //   break;
-    // }
-    // else {
-    //   updateStatus(">> [ais] socket0: send failed.");
-    //   if (++rt > 5) {
-    //     delay(100);
-    //     ESP.deepSleep(1e6);
-    //     delay(100);
-    //     break;
-    //   }
-    // }
+  // while (true) {
+  // updateStatus("dispatching queue...");
+  // if (nb.sendMessageHex(buffer, buflen, 0)) {
+  //   updateStatus(">> [ais] socket0: send ok.");
+  //   lastSentOkMillis = millis();
+  //   nbSentOk++;
+  //   delay(100);
+  //   break;
+  // }
+  // else {
+  //   updateStatus(">> [ais] socket0: send failed.");
+  //   if (++rt > 5) {
+  //     delay(100);
+  //     ESP.deepSleep(1e6);
+  //     delay(100);
+  //     break;
+  //   }
+  // }
+}
+
+void CMMC_Modem::sendPacket(uint8_t *text, int buflen) {
+  int rt = 0;
+  uint8_t buffer[buflen];
+  memcpy(buffer, text, buflen);
+  // char buffer[buflen*2+1];
+  // bzero(buffer, strlen(buffer));
+  //  for (int x = 0; x < buflen*2; x+= 2) {
+  //   sprintf(buffer[x], "%02x", text[x]);
+  // }
+  // Serial.printf("buflen=%d, ", buflen);
+  // Serial.println(buffer);
+  while (true) {
+    updateStatus("dispatching queue...");
+    if (nb->sendMessageHex(buffer, buflen, 0)) {
+      updateStatus(">> [ais] socket0: send ok.");
+      lastSentOkMillis = millis();
+      // nbSentOk++;
+      delay(100);
+      break;
+    }
+    else {
+      updateStatus(">> [ais] socket0: send failed.");
+      if (++rt > 5) {
+        delay(100);
+        ESP.deepSleep(1e6);
+        delay(100);
+        break;
+      }
+    }
+    delay(200);
+  }
 }
 
 String CMMC_Modem::getStatus() {
--- lib/CMMC_Modem/CMMC_Modem.h
+++ lib/CMMC_Modem/CMMC_Modem.h
@@ -14,6 +14,26 @@ class CMMC_Modem: public CMMC_Module{
     void updateStatus(String s);
     // void resetModem();
     void sendPacket(uint8_t *text, int buflen);
+
+    // CMMC_PACKET_T pArr[60];
+    int pArrIdx = 0;
+    uint32_t lastSentOkMillis = 0;
+    unsigned int ct = 1;
+    static char msgId[5];
+    IPAddress ip = IPAddress(103, 20, 205, 85);
+    uint8_t _buffer[1300];
+    float  batt;
+    float  batt_raw;
+    float  batt_percent;
+    int analogValue;
+    CMMC_Interval keepAliveInterval;
+    char latC[20];
+    char lngC[20];
+    char latlngC[40];
+
+    uint8_t currentSleepTimeMinuteByte = 30;
+    uint32_t msAfterESPNowRecv = millis();
+
   public:
     CMMC_Modem(Stream*);
     void hello();
