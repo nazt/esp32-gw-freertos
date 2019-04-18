fff8398ebcda0d74ea09f505030863260ab9bc1a
Nat <nat@cmmc.io>
2019-04-18 08:45:07 +0700

    remove gps unused lines of code

--- lib/CMMC_GPS/CMMC_GPS.cpp
+++ lib/CMMC_GPS/CMMC_GPS.cpp
@@ -35,17 +35,7 @@ void CMMC_GPS::loop() {
   Serial.printf("wait GPS_SERIAL for %lums\r\n", millis() - ms);
   while (serial->available() > 0) {
     char c = serial->read();
-    // Serial.print(".");
     if (gps.encode(c)) {
-      // Serial.printf("year=%d\r\n", gps.date.year()); // Year (2000+) (u16)
-      // Serial.printf("month=%d\r\n", gps.date.month()); // Month (1-12) (u8)
-      // Serial.printf("day=%d\r\n", gps.date.day()); // Day (1-31) (u8)
-      // Serial.printf("%d\r\n", gps.time.value()); // Raw time in HHMMSSCC format (u32)
-      // Serial.printf("hour=%d\r\n", gps.time.hour()); // Hour (0-23) (u8)
-      // Serial.printf("minute=%d\r\n", gps.time.minute()); // Minute (0-59) (u8)
-      // Serial.printf("second=%d\r\n", gps.time.second()); // Second (0-59) (u8)
-      // Serial.printf("centisecond=%d\r\n", gps.time.centisecond()); // 100ths of a second (0-99) (u8)
-
       if (gps.location.isValid()) {
         gpsNoSignal = 0;
         // 5Minute
@@ -61,27 +51,14 @@ void CMMC_GPS::loop() {
               Serial.println(latlngC);
               this->_lastFetchLocation = millis();
         }
+
         if (gps.time.isUpdated()) {
-            // Serial.print(F("Date:  "));
-            // Serial.print(gps.date.year());
-            // Serial.print(F("/"));
-            // Serial.print(gps.date.month());
-            // Serial.print(F("/"));
-            // Serial.println(gps.date.day());
-            // Serial.print(F("TIME:  "));
-            // Serial.print(gps.time.hour()+7 % 24);
-            // Serial.print(F(":"));
-            // Serial.print(gps.time.minute());
-            // Serial.print(F(":"));
-            // Serial.println(gps.time.second());
-            // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
             if (!_lastSyncRtc || millis() - _lastSyncRtc > 5*1000) {
               Serial.println("[[[SYNC TIME WITH GPS]]]!!!");
               rtc->adjust(DateTime(gps.date.year(), gps.date.month(), gps.date.day(), gps.time.hour()+7%24, gps.time.minute(), gps.time.second()));
               delay(200);
               _lastSyncRtc = millis();
             }
-            // delay(200);/
         }
       }
       else
@@ -91,11 +68,7 @@ void CMMC_GPS::loop() {
         strcat(latlngC, "0.0000");
         strcat(latlngC, ",");
         strcat(latlngC, "0.0000");
-        // Serial.println(F("INVALID GPS DATA"));
-        // delay(100);
       }
     }
-    // delay(1);
-    // Serial.println("-");
   }
 }
